be3fee0c60c49043d9b275c75895f3d9f2d0ddbf
Ndoda Kheswa (ZA) <Ndoda.Kheswa@absa.africa>
2021-05-17 22:44:05 +0200

    adding login

--- src/main/kotlin/com/learncodingrsa/lambdaecommerce/services/AuthenticationService.kt
+++ src/main/kotlin/com/learncodingrsa/lambdaecommerce/services/AuthenticationService.kt
@@ -1,17 +1,15 @@
 package com.learncodingrsa.lambdaecommerce.services
 
-import com.learncodingrsa.lambdaecommerce.model.UserInfo
-import com.learncodingrsa.lambdaecommerce.model.UserInfoResponse
+import com.learncodingrsa.lambdaecommerce.model.*
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import software.amazon.awssdk.services.cognitoidentity.model.CognitoIdentityProvider
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient
 import software.amazon.awssdk.services.cognitoidentityprovider.model.*
-import software.amazon.awssdk.services.iam.model.CreateUserRequest
 
 @Service
 class AuthenticationService(private val client: CognitoIdentityProviderClient,
-                            @Value("\${cognito.pool-id}") private val poolId: String) : AuthenticationInterface {
+                            @Value("\${cognito.pool-id}") private val poolId: String,
+                            @Value("\${cognito.client-id}") private val clientId: String) : AuthenticationInterface {
 
     fun createNewUser(userInfo: UserInfo?) : String {
         try {
@@ -47,6 +45,88 @@ class AuthenticationService(private val client: CognitoIdentityProviderClient,
         }
         return poolId
     }
+
+    /**
+     * fun login(userName: String, password: String) : LoginInfo? {
+    var info: LoginInfo? = null
+    var sessionInfo: SessionInfo? = sessionHandler(userName, password)
+
+    if (sessionInfo != null) {
+    val userInfo: UserInfo? = getUserInfo()
+    }
+    }
+     */
+
+    fun changeTemporaryPassword(passwordRequest: PasswordRequest) : AdminRespondToAuthChallengeResponse? {
+        var challengeResponse: AdminRespondToAuthChallengeResponse? = null
+
+        try {
+            var sessionInfo: SessionInfo? = sessionHandler(passwordRequest.userName, passwordRequest.oldPassword)
+            val sessionString = sessionInfo?.let { it.session }
+
+            if (!sessionString.isNullOrEmpty()) {
+                val challengeResponses = hashMapOf<String, String>()
+                challengeResponses[USERNAME] = passwordRequest.userName
+                challengeResponses[PASSWORD] = passwordRequest.oldPassword
+                challengeResponses[NEW_PASSWORD] = passwordRequest.newPassword
+
+                challengeResponse = client.adminRespondToAuthChallenge(
+                    AdminRespondToAuthChallengeRequest
+                        .builder()
+                        .challengeName(ChallengeNameType.NEW_PASSWORD_REQUIRED)
+                        .challengeResponses(challengeResponses)
+                        .clientId(clientId)
+                        .userPoolId(poolId)
+                        .session(sessionString)
+                        .build()
+                )
+            }
+        } catch (e: CognitoIdentityProviderException) {
+            e.awsErrorDetails().errorMessage()
+        }
+
+        return challengeResponse
+    }
+
+    fun sessionHandler(userName: String, password: String) : SessionInfo? {
+        var info: SessionInfo? = null
+        try {
+
+            val authParams = mutableMapOf<String, String>()
+            authParams["USERNAME"] = userName
+            authParams["PASSWORD"] = password
+
+            val authResult = client.adminInitiateAuth(
+                AdminInitiateAuthRequest.builder()
+                    .authFlow(AuthFlowType.ADMIN_NO_SRP_AUTH)
+                    .userPoolId(poolId)
+                    .clientId(clientId)
+                    .authParameters(authParams)
+                    .build()
+            )
+
+            if (authResult != null) {
+                val session = authResult.session()
+                var accessToken: String? = null
+                val resultType = authResult.authenticationResult()
+                if (resultType != null) {
+                    accessToken = resultType.accessToken()
+                }
+                val challengeResult = authResult.challengeName().name
+                info = accessToken?.let { SessionInfo(session, it, challengeResult) }
+            }
+        } catch (e: CognitoIdentityProviderException) {
+            e.awsErrorDetails().errorMessage()
+        }
+
+        return info
+    }
+
+    /**
+     * fun getUserInfo(userName: String) {
+
+    }
+     */
 
     fun findUserByEmailAddress(email: String?) : UserInfoResponse? {
         var info: UserInfoResponse? = null
@@ -92,6 +172,12 @@ class AuthenticationService(private val client: CognitoIdentityProviderClient,
             }
         }
         return info
+    }
+
+    companion object {
+        private val USERNAME: String = "USERNAME"
+        private val PASSWORD: String = "PASSWORD"
+        private val NEW_PASSWORD: String = "NEW_PASSWORD"
     }
 
 }
